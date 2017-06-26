b09e2f0bbf5f924980889837679fafcca0864d9c
Ethan Wolkowicz <ethan.wolkowicz@teamaol.com>
2017-06-26 15:50:57 -0400

    initial commit of code - no testing yet

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# terraform-remote-state
-Helper script to manage terraform state files
+# Terraform S3 Remote State Setup
+Helper script to manage terraform state files in AWS S3
+
+#### Description: Sets up remote statefile. Should be run before apply or destroy.
+When you run this it will setup the path as the same name of the folder of your terraform template.
+
+ex: if you path is /home/user/application/terraform_application then the s3 folder will be called terraform_application
+
+Note:  It's helpful to copy this to one of your ENV paths so you can call it directly.  This was originally designed to be used in a Jenkins pipeline, but can be used locally if needed.
+
+
+
+#### Usage: setup-terraform-remote.sh [module_path] [region] [bucket_name] [application] [env]
+
+module_path:
+ - the path of the terraform module
+
+region:
+ - the region of the s3 bucket (us-east-1 etc..)
+
+bucket_name:
+ - the name of the s3 bucket to store the config
+
+application:
+ - the name of the application
+
+environment:
+  - dev
+  - test
+  - prod
---
+++ terraform-remote-state.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -e
+
+module_path="$1"
+terraform_bucket_region="$2"
+terraform_bucket_name="$3"
+application="$4"
+environment="$5"
+
+function usage() {
+  echo "Description: Sets up remote statefile. Should be run before apply or destroy."
+  echo "Usage: terraform.sh [module_path] [bucket_region] [bucket_name] [application] [environment]"
+  echo
+  echo "module_path:"
+  echo " - the path of the terraform module"
+  echo
+  echo "region:"
+  echo " - the aws region (ex. us-east-1)"
+  echo
+  echo "bucket_name:"
+  echo " - name of the s3 bucket to store the state in"
+  echo
+  echo "Application:"
+  echo " - Application/Service Name"
+  echo
+  echo "Environment:"
+  echo " - dev"
+  echo " - test"
+  echo " - prod"
+}
+
+# Ensure script console output is separated by blank line at top and bottom to improve readability
+trap echo EXIT
+echo
+
+# Validate the input arguments
+if [ "$#" -lt 4 ]; then
+  usage
+  exit 1
+fi
+
+# Get the absolute path to the module
+if [[ "$module_path" != /* ]]; then
+  module_path=$(cd "$(pwd)/$module_path" && pwd)
+else
+  module_path=$(cd "$module_path" && pwd)
+fi
+
+# Clear the .terraform directory (we want to pull the state from the remote)
+rm -rf "$module_path/.terraform"
+
+# Make sure we're running in the module directory
+cd "$module_path"
+
+tfstate="s3://$terraform_bucket_name/$application/$environment.tfstate"
+tfversion='0.9.8'
+
+if [[ -z "`aws s3 ls $tfstate`" ]]; then
+  echo "No previous state found"
+else
+  if [[ -z "`aws s3 cp $tfstate - | jq '.modules [] .resources []'`" ]]; then
+    aws s3 rm $tfstate
+  else
+    tfversion="`aws s3 cp $tfstate - | jq '.terraform_version' | tr -d '\"'`"
+  fi
+fi
+
+if [[ "`uname`" == "Darwin" ]]; then
+  bin="darwin_amd64"
+else
+  bin="linux_amd64"
+fi
+curl https://releases.hashicorp.com/terraform/$tfversion/terraform_${tfversion}_${bin}.zip > terraform.zip
+unzip -o terraform.zip
+chmod +x terraform
+
+# Configure remote state storage
+./terraform init \
+  -backend=true \
+  -backend-config="region=$terraform_bucket_region" \
+  -backend-config="bucket=$terraform_bucket_name" \
+  -backend-config="key=$application/$environment.tfstate" \
+  -force-copy -get=true -input=false
